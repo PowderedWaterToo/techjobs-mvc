1b246aef60f55e21edf300bd77e0aa259069fe55
Michael Porter <mb_porter@outlook.com>
2018-06-13 19:24:57 -0500

    3.11 prep work added in, changing ArrayLists to Iterable or List

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by LaunchCode
@@ -28,14 +29,14 @@ public class SearchController {
     public String results(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
 
         if (searchType.equals("all")) {
-            ArrayList<HashMap<String, String>> jobs = JobData.findByValue(searchTerm);
+            List<HashMap<String, String>> jobs = JobData.findByValue(searchTerm);
             model.addAttribute("columns", ListController.columnChoices);
             model.addAttribute("title", "Jobs that contain the word(s): " + searchTerm);
             model.addAttribute("jobs", jobs);
             return "search";
         }
         else {
-            ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            List<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
             model.addAttribute("columns", ListController.columnChoices);
             model.addAttribute("title",  "Jobs where " + searchType + " contains the word(s): " + searchTerm);
             model.addAttribute("jobs", jobs);
